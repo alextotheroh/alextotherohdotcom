fab7977e3e0feaea76d852c54dd0fe90c11f8a70
Alex Totheroh <alextotheroh@gmail.com>
2018-08-05 12:27:49 -0700

    first commit, used create-react-app to initialize app

---
+++ src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to React</h1>
+        </header>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
